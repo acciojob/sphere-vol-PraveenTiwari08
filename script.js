b93d51eae551b94a4f64a6a2a1bbb9680ae5eb15
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-05-06 23:35:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function volume_sphere() {
+function volume_sphere(radiusVal) {
     //Write your code here
+
+	 if (isNaN(radiusVal) || radiusVal <= 0) {
+        return NaN; // Return NaN if the input is not a number or is negative
+    }
 	let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
     return volume;
   
 } 
+
+let radius = document.document.getElementById("radius");
+let radiusVal = parseFloat(radius.value)
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
